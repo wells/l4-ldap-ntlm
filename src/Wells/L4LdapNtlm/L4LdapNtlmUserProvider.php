5197d55158a649c0eae16f5d9aaaee39183611d4
Brian Wells <brian.wells@untoldvoyage.com>
2014-05-19 10:29:53 -0600

    [BugFix] Token "Support"
    Tokens are currently not implemented for this package. The package
    should not fail if "remember me" tokens are attempted to be used.
    Support should arrive in the next iteration.

--- src/Wells/L4LdapNtlm/L4LdapNtlmUserProvider.php
+++ src/Wells/L4LdapNtlm/L4LdapNtlmUserProvider.php
@@ -57,6 +57,32 @@ class L4LdapNtlmUserProvider implements UserProviderInterface
 	}
 
 	/**
+	 * Retrieve a user by by their unique identifier and "remember me" token.
+	 *
+	 * @param  mixed  $identifier
+	 * @param  string  $token
+	 * @return \Illuminate\Auth\UserInterface|null
+	 */
+	public function retrieveByToken($identifier, $token)
+	{
+		return $this->retrieveByID($identifier);
+	}
+
+	/**
+	 * Update the "remember me" token for the given user in storage.
+	 *
+	 * @param  \Illuminate\Auth\UserInterface  $user
+	 * @param  string  $token
+	 * @return void
+	 */
+	public function updateRememberToken(UserInterface $user, $token)
+	{
+		// AD has no token field
+		// Will implement a user table in a db for next iteration
+		//$user->setRememberToken($token);
+	}
+
+	/**
 	* Retrieve a user by their unique identifier.
 	*
 	* @param  mixed  $identifier
